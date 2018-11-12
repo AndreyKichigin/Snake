d0046c64dc0bd3db0049b56663f282f9ba7e5b9a
Andrey Kichigin <kichigin-av@mail.ru>
2018-11-12 23:34:32 +0500

    Create new project

---
+++ src/main/java/snake/game/avkichigin/ru/spacesnake/CanvasView.java
@@ -0,0 +1,28 @@
+package snake.game.avkichigin.ru.spacesnake;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CanvasView extends View {
+    private static final int COLOR= Color.BLUE;
+
+    public CanvasView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        Paint paint=new Paint();
+        paint.setColor(COLOR);
+        canvas.drawCircle(100, 100, 30, paint);
+
+        
+    }
+}
